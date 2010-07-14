80bd91ca1ef0810a680977da635564e97a2df1d9
robertj <robert.j@plan-tre.net>
2010-07-14 14:46:30 +0200

    Version Update

--- AssemblyInfoGData.cs
+++ AssemblyInfoGData.cs
@@ -9,4 +9,5 @@ using System.Security.Permissions;
 [assembly: AssemblyCompany("plan-tre.net")]
 [assembly: AssemblyCopyright("Robert Jönsson")]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = 0)]
-[assembly: AssemblyVersion("0.5.0.2")]
+[assembly: AssemblyVersion("0.5.0.3")]
+
