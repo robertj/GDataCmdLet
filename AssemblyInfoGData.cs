b27f268584257de1de4a4889790722411c22dab9
robertj <robert.j@plan-tre.net>
2010-07-19 14:13:56 +0200

    Added support for group owner

--- AssemblyInfoGData.cs
+++ AssemblyInfoGData.cs
@@ -9,5 +9,6 @@ using System.Security.Permissions;
 [assembly: AssemblyCompany("plan-tre.net")]
 [assembly: AssemblyCopyright("Robert Jönsson")]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = 0)]
-[assembly: AssemblyVersion("0.5.0.5")]
+[assembly: AssemblyVersion("0.5.0.6")]
+
 
